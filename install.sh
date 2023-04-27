a1015019f031258750d07bf3ce838c2583bda6ec
ruizii <santiago-rb1@outlook.es>
2023-04-27 18:16:43 -0500

    added tmux

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo -e "\n${BLUE}Instalando dotfiles en:${ENDCOLOR}"
 echo -e "\n\t${CYAN}${HOME}/.config/kitty/kitty.conf${ENDCOLOR}"
 echo -e "\t${CYAN}${HOME}/.config/nvim/init.vim${ENDCOLOR}"
 echo -e "\t${CYAN}${HOME}/.zshrc${ENDCOLOR}"
+echo -e "\t${CYAN}${HOME}/.tmux.conf"
 
 mkdir -p ${HOME}/.config/kitty
 mkdir -p ${HOME}/.config/nvim
@@ -15,3 +16,4 @@ mkdir -p ${HOME}/.config/nvim
 ln -s $(pwd)/kitty.conf ${HOME}/.config/kitty/kitty.conf
 ln -s $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
 ln -s $(pwd)/zshrc ${HOME}/.zshrc
+ln -s $(pwd)/tmux.conf ${HOME}/.tmux.conf
