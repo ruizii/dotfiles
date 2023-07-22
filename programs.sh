d4a5e6cf513cb700e06b36a2bb02f560760747ef
ruizii <santiago-rb1@outlook.es>
2023-07-22 14:01:03 -0500

    Program install script

--- programs.sh
+++ programs.sh
@@ -1,1 +1,6 @@
-aaaa
+sudo apt install kitty npm python3-venv bat ripgrep node golang
+
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.3/bottom_0.9.3_amd64.deb
+sudo dpkg -i bottom_0.9.3_amd64.deb
+
+curl -sS https://starship.rs/install.sh | sh
