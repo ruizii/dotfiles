f7ccf2a5eccd501626bbd9996f87252cbc34b31b
ruizii <95191116+ruizii@users.noreply.github.com>
2023-07-26 18:38:59 -0500

    Delete programs.sh

--- programs.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install kitty npm python3-venv bat ripgrep nodejs golang xclip
-
-curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.3/bottom_0.9.3_amd64.deb
-sudo dpkg -i bottom_0.9.3_amd64.deb
-rm bottom_0.9.3_amd64.deb
-
-curl -sS https://starship.rs/install.sh | sh
