b2493d9a407bf89131928debdc3f8b0e99ff5d62
ruizii <santiago-rb1@outlook.es>
2023-07-26 18:47:42 -0500

    ctrl+c

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 BLUE='\033[0;34m'
+RED='\033[0;31m'
 ENDCOLOR='\033[0m'
+
+function ctrl_c() {
+	echo -e "${RED}\n\t[!] Saliendo...${ENDCOLOR}\n\n"
+	rm -rf CascadiaCode.zip
+	tput cnorm
+	exit 1
+}
+
+trap ctrl_c SIGINT
+
+tput civis
 
 sudo apt install kitty npm python3-venv bat ripgrep nodejs golang composer
 
-echo -e "${BLUE}\n\t[+] Instalando Nerd Font...${ENDCOLOR}"
+echo -e "${BLUE}\n\t[+] Instalando Nerd Font${ENDCOLOR}"
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip &>/dev/null
 mkdir font
@@ -15,10 +27,14 @@ cp ./font/CaskaydiaCoveNerdFont-Regular.ttf ~/.local/share/fonts/
 rm -rf font/
 
 echo -e "${BLUE}\n\t[+] Instalando Bottom"
+sleep 0.5
 
 curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.3/bottom_0.9.3_amd64.deb
 sudo dpkg -i bottom_0.9.3_amd64.deb
 rm bottom_0.9.3_amd64.deb
 
 echo -e "${BLUE}\n\t[+] Instalando Starship"
+sleep 0.5
 curl -sS https://starship.rs/install.sh | sh
+
+tput cnorm
