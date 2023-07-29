6151b3f94fa30768ba9e9b8d635030921b55f98b
ruizii <95191116+ruizii@users.noreply.github.com>
2023-07-28 21:39:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ echo -e "${BLUE}\n\t[+] Instalando Nerd Font${ENDCOLOR}"
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip &>/dev/null
 mkdir font
+mkdir -p ~/.local/share/fonts
 mv CascadiaCode.zip font/
 unzip ./font/CascadiaCode.zip -d ./font/ &>/dev/null
 cp ./font/CaskaydiaCoveNerdFont-Regular.ttf ~/.local/share/fonts/
