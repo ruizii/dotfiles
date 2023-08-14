642f71fb1e369903117aa30825f84339203628ae
ruizii <santiago-rb1@outlook.es>
2023-08-14 14:02:17 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -15,27 +15,18 @@ trap ctrl_c SIGINT
 
 tput civis
 
-sudo apt install kitty npm python3-venv bat ripgrep nodejs golang composer luarocks exa
+# List
+#
+# kitty npm python3-venv bat ripgrep nodejs golang composer luarocks exa
 
-echo -e "${BLUE}\n\t[+] Instalando Nerd Font${ENDCOLOR}"
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip &>/dev/null
-mkdir font
-mkdir -p ~/.local/share/fonts
-mv CascadiaCode.zip font/
-unzip ./font/CascadiaCode.zip -d ./font/ &>/dev/null
-cp ./font/CaskaydiaCoveNerdFont-Regular.ttf ~/.local/share/fonts/
-rm -rf font/
-
-echo -e "${BLUE}\n\t[+] Instalando Bottom"
+echo -e "${BLUE}\n\t[+] Instalando Bottom${ENDCOLOR}"
 sleep 0.5
 
-curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.3/bottom_0.9.3_amd64.deb
+curl -s -LO https://github.com/ClementTsang/bottom/releases/download/0.9.3/bottom_0.9.3_amd64.deb
 sudo dpkg -i bottom_0.9.3_amd64.deb
 rm bottom_0.9.3_amd64.deb
 
-echo -e "${BLUE}\n\t[+] Instalando Starship"
+echo -e "${BLUE}\n\t[+] Instalando Starship${ENDCOLOR}"
 sleep 0.5
-curl -sS https://starship.rs/install.sh | sh
-
 tput cnorm
+curl -sS https://starship.rs/install.sh | sh
