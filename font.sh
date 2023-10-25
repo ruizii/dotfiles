032301b820648af50614a40a00657531b06c6707
ruizii <95191116+ruizii@users.noreply.github.com>
2023-10-25 13:55:37 -0500

    Delete font.sh

--- font.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip &>/dev/null
-mkdir font
-mkdir -p ~/.local/share/fonts
-mv CascadiaCode.zip font/
-unzip ./font/CascadiaCode.zip -d ./font/ &>/dev/null
-cp ./font/CaskaydiaCoveNerdFont-Regular.ttf ~/.local/share/fonts/
-rm -rf font/
