9a8066acbeaebfd4a0e196370b1afe4a4d1fbb7d
ruizii <santiago-rb1@outlook.es>
2024-10-07 23:38:21 -0500

    Add zshconfig

--- zsh/.config/zsh/config.d/fzf.zsh
+++ zsh/.config/zsh/config.d/fzf.zsh
@@ -3,22 +3,22 @@ _fzf_compgen_dir() {
 }
 
 _fzf_compgen_path() {
-  fd --type f --hidden --color=always --follow --exclude ".git" . "$1"
+    fd --type f --hidden --color=always --follow --exclude ".git" . "$1"
 }
 
 _fzf_comprun() {
-  local command=$1
-  shift
-  case "$command" in
-    cd)           fzf --preview 'eza -T --color=always --icons=always {} | head -200' "$@" ;;
-    export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
-    ssh)          fzf --preview 'dig {}'                   "$@" ;;
-    *)
-        if [[ -f /etc/debian_version ]]; then
-            fzf --preview 'batcat -n --color=always {}' "$@"
-        else
-            fzf --preview 'bat -n --color=always {}' "$@"
-        fi
-    ;;
-  esac
+    local command=$1
+    shift
+    case "$command" in
+        cd)           fzf --preview 'eza -T --color=always --icons=always {} | head -200' "$@" ;;
+        export|unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
+        ssh)          fzf --preview 'doggo {}' "$@" ;;
+        *)
+            if [[ -f /etc/arch-release ]]; then
+                fzf --preview 'bat -n --color=always {}' "$@"
+            else
+                fzf --preview 'batcat -n --color=always {}' "$@"
+            fi
+        ;;
+    esac
 }
