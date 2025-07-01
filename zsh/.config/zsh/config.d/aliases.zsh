76c5fd2f853fc15e314f22ed683b52c8371a926c
ruizii <santiago-rb1@outlook.es>
2025-07-01 18:39:38 -0500

    Add listen alias

--- zsh/.config/zsh/config.d/aliases.zsh
+++ zsh/.config/zsh/config.d/aliases.zsh
@@ -23,6 +23,7 @@ alias curl='curlie -v'
 alias rmtest='cd ~ && rm -rf ./test/'
 alias nvconfig="cd $HOME/.config/nvim && nvim"
 alias scan="shitty-scanner.sh $@"
+alias listen="shitty-revshell-listener.sh $@"
 alias zshconfig="cd $ZDOTDIR && nvim"
 alias gdb='gdb -q'
 alias less='bat'
